9aadb596162a92d00e2d9c11db3c8d505e2fb12f
ggm <godgunman@gmail.com>
2014-10-23 20:53:52 +0800

    add properties to each view

--- SimpleLinearLayout/src/com/example/simplelinearlayout/MainActivity.java
+++ SimpleLinearLayout/src/com/example/simplelinearlayout/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.simplelinearlayout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -21,10 +23,32 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		// setContentView(R.layout.activity_main);
 		linearLayout = new LinearLayout(this);
+		linearLayout.setOrientation(LinearLayout.VERTICAL);
+		linearLayout.setPadding(15, 15, 15, 15);
+
 		editText1 = new EditText(this);
+		editText1.setHint("To");
+
 		editText2 = new EditText(this);
+		editText2.setHint("Subject");
+
+		LinearLayout.LayoutParams messageParams = new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.MATCH_PARENT, 0);
+		messageParams.weight = 1;
+
 		editText3 = new EditText(this);
+		editText3.setHint("Message");
+		editText3.setGravity(Gravity.TOP);
+		editText3.setLayoutParams(messageParams);
+
+		LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.WRAP_CONTENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT);
+		buttonParams.gravity = Gravity.RIGHT;
+
 		button = new Button(this);
+		button.setText("Send");
+		button.setLayoutParams(buttonParams);
 
 		linearLayout.addView(editText1);
 		linearLayout.addView(editText2);
