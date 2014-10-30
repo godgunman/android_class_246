0dd944ef35f0805a22e92c07f44358272e40ad13
godgunman <godgunman@gmail.com>
2014-10-30 21:23:39 +0800

    add writeFile method

--- firstapp/src/com/example/firstapp/MessageActivity.java
+++ firstapp/src/com/example/firstapp/MessageActivity.java
@@ -1,6 +1,11 @@
 package com.example.firstapp;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -10,10 +15,30 @@ public class MessageActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_message);
-		
+
 		String text = getIntent().getStringExtra("text");
 		boolean isChecked = getIntent().getBooleanExtra("checkBox", false);
-		
+
 		Log.d("debug", "extra:" + text + "," + isChecked);
+		writeFile(text);
 	}
+
+	private void writeFile(String text) {
+		text += "\n";
+		
+		try {
+			FileOutputStream fos = openFileOutput("message.txt",
+					Context.MODE_APPEND);
+			fos.write(text.getBytes());
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 }
