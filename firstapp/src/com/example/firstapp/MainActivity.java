7d8bd78c07836c6f2f5fd0fa01edfc8cb68e9588
godgunman <godgunman@gmail.com>
2014-11-13 19:44:47 +0800

    use fragment

--- firstapp/src/com/example/firstapp/MainActivity.java
+++ firstapp/src/com/example/firstapp/MainActivity.java
@@ -1,123 +1,29 @@
 package com.example.firstapp;
 
+import com.example.firstapp.fragment.InputFragment;
 import com.parse.Parse;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
-	private TextView textView;
-	private EditText editText;
-	private Button button;
-	private SharedPreferences sp;
-	private SharedPreferences.Editor editor;
-	private CheckBox checkBox;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		Parse.initialize(this, "j0mdZulYSEQYwHcqA0JwRwzIRfY4U0TNtV6Gw2Uz", "z7vFTTlQjLRMkVzNaCjIDMD5R1KqzQkoRzqBZBoS");
 
-		sp = getSharedPreferences("settings", Context.MODE_PRIVATE);
-		editor = sp.edit();
+		Parse.initialize(this, "j0mdZulYSEQYwHcqA0JwRwzIRfY4U0TNtV6Gw2Uz",
+				"z7vFTTlQjLRMkVzNaCjIDMD5R1KqzQkoRzqBZBoS");
 
-		textView = (TextView) findViewById(R.id.textView1);
-		editText = (EditText) findViewById(R.id.editText1);
-		button = (Button) findViewById(R.id.button1);
-		checkBox = (CheckBox) findViewById(R.id.checkBox1);
+		InputFragment inputFragment = new InputFragment();
 
-		textView.setText("Hi world!");
-		button.setText("submit");
-		button.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				submit();
-			}
-		});
-
-		editText.setOnKeyListener(new OnKeyListener() {
-
-			@Override
-			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				String text = editText.getText().toString();
-
-				editor.putString("text", text);
-				editor.commit();
-
-				if (keyCode == KeyEvent.KEYCODE_ENTER
-						&& event.getAction() == KeyEvent.ACTION_DOWN) {
-					submit();
-					return true;
-				}
-
-				return false;
-			}
-		});
-
-		checkBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView,
-					boolean isChecked) {
-				editor.putBoolean("checkBox", isChecked);
-				editor.commit();
-			}
-		});
-
-		loadSettings();
-	}
-
-	private void loadSettings() {
-		String text = sp.getString("text", "");
-		editText.setText(text);
-
-		checkBox.setChecked(sp.getBoolean("checkBox", false));
-	}
-
-	private void submit() {
-		String text = editText.getText().toString();
-		if (checkBox.isChecked()) {
-			text = "*********";
-		}
-
-		Toast.makeText(this, text, Toast.LENGTH_LONG).show();
-		editText.setText("");
-
-		editor.putString("text", "");
-		editor.commit();
-
-		Intent intent = new Intent();
-		intent.setClass(this, MessageActivity.class);
-		intent.putExtra("text", text);
-		intent.putExtra("checkBox", checkBox.isChecked());
-		startActivity(intent);
-	}
-
-	public void clickButton(View view) {
-		Log.d("debug", "on click");
-
-		String newText = editText.getText().toString();
-		textView.setText(newText);
+		FragmentTransaction ft = getFragmentManager().beginTransaction();
+		ft.add(R.id.container, inputFragment);
+		ft.commit();
 	}
 
 	@Override
---
+++ firstapp/src/com/example/firstapp/fragment/InputFragment.java
@@ -0,0 +1,128 @@
+package com.example.firstapp.fragment;
+
+import com.example.firstapp.MessageActivity;
+import com.example.firstapp.R;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+public class InputFragment extends Fragment {
+
+	private TextView textView;
+	private EditText editText;
+	private Button button;
+	private CheckBox checkBox;
+
+	private SharedPreferences sp;
+	private SharedPreferences.Editor editor;
+
+	public InputFragment() {
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		sp = getActivity().getSharedPreferences("settings",
+				Context.MODE_PRIVATE);
+		editor = sp.edit();
+
+		View rootView = inflater.inflate(R.layout.fragment_input, container);
+
+		textView = (TextView) rootView.findViewById(R.id.textView1);
+		editText = (EditText) rootView.findViewById(R.id.editText1);
+		button = (Button) rootView.findViewById(R.id.button1);
+		checkBox = (CheckBox) rootView.findViewById(R.id.checkBox1);
+
+		textView.setText("Hi world!");
+		button.setText("submit");
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				submit();
+			}
+		});
+
+		editText.setOnKeyListener(new OnKeyListener() {
+
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				String text = editText.getText().toString();
+
+				editor.putString("text", text);
+				editor.commit();
+
+				if (keyCode == KeyEvent.KEYCODE_ENTER
+						&& event.getAction() == KeyEvent.ACTION_DOWN) {
+					submit();
+					return true;
+				}
+
+				return false;
+			}
+		});
+
+		checkBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				editor.putBoolean("checkBox", isChecked);
+				editor.commit();
+			}
+		});
+
+		loadSettings();
+		return rootView;
+	}
+
+	private void loadSettings() {
+		String text = sp.getString("text", "");
+		editText.setText(text);
+
+		checkBox.setChecked(sp.getBoolean("checkBox", false));
+	}
+
+	private void submit() {
+		String text = editText.getText().toString();
+		if (checkBox.isChecked()) {
+			text = "*********";
+		}
+
+		Toast.makeText(getActivity(), text, Toast.LENGTH_LONG).show();
+		editText.setText("");
+
+		editor.putString("text", "");
+		editor.commit();
+
+		Intent intent = new Intent();
+		intent.setClass(getActivity(), MessageActivity.class);
+		intent.putExtra("text", text);
+		intent.putExtra("checkBox", checkBox.isChecked());
+		startActivity(intent);
+	}
+
+	public void clickButton(View view) {
+
+		String newText = editText.getText().toString();
+		textView.setText(newText);
+	}
+}
